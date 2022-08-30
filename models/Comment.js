4d8e136235379055bb68ffbaae83bc64910136b6
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-08-30 10:42:36 -0500

    finished replies

--- models/Comment.js
+++ models/Comment.js
@@ -1,4 +1,32 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+
+const ReplySchema = new Schema(
+  {
+    //set custom id to avoid confusion with parent comment _id
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    replyBody: {
+      type: String
+    },
+    writtenBy: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+)
 
 const CommentSchema = new Schema({
   writtenBy: {
@@ -10,7 +38,21 @@ const CommentSchema = new Schema({
   createdAt: {
     type: Date,
     default: Date.now,
+    get: createdAtVal => dateFormat(createdAtVal)
   },
+  replies: [ReplySchema]
+},
+{
+  toJSON: {
+    getters: true,
+    virtuals: true
+  },
+  id: false
+}
+);
+
+CommentSchema.virtual('replyCount').get(function() {
+  return this.replies.length;
 });
 
 const Comment = model("Comment", CommentSchema);
